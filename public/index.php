ee324a7552e1a49e73da54e00c59dcaa83b01e08
cyxSetyo <98861415+cyxSetyo@users.noreply.github.com>
2022-11-09 09:32:33 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use BelajarPhpMvc\Belajar\PHP\MVC\App\Router;
 
 Router::add('GET', '/', 'HomeController', 'index');
-Router::add('GET', '/login', 'UserController', 'index');
+Router::add('GET', '/login', 'UserController', 'login');
+Router::add('GET', '/register', 'UserController', 'register');
 
 Router::run();
